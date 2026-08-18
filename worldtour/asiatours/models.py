024e0dd883390c0f64016f60245bcab90904e2ae
Prashant Subedi <subediprashant76@gmail.com>
2026-08-18 19:37:17 +0545

    adding models

--- worldtour/asiatours/models.py
+++ worldtour/asiatours/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Tour(models.Model):
     #we need a origin country,dest,no of nights, price for the tour
+    name=models.CharField(max_length=40)
     origin_country=models.CharField(max_length=50)
     dest_country=models.CharField(max_length=50)
     no_of_nights=models.IntegerField()
@@ -13,7 +14,7 @@ class Tour(models.Model):
 
     #This is a string representation of the tours
     def __str__(self):
-        return f"ID:{self.id}: You are travelling from {self.origin_country} to {self.dest_country} for {self.no_of_nights} nights and total price of {self.price}"
+        return f"ID:{self.id}: {name} is travelling from {self.origin_country} to {self.dest_country} for {self.no_of_nights} nights and total price of {self.price}"
 
 
 
